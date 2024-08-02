f4c6f2b6c402cd8ac1047f24f1f123e73f92f663
George Bokoev <147842599+BokGosha@users.noreply.github.com>
2024-08-02 21:49:29 +0300

    Изменить имя пакета configuration

--- src/main/java/tbank/project/configuration/RestTemplateConfig.java
+++ src/main/java/tbank/project/configuration/RestTemplateConfig.java
@@ -1,4 +1,4 @@
-package tbank.project.Configuration;
+package tbank.project.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
